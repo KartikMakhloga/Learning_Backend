81d4934a04412e4e486602da70db71e3e69239cc
kartik <kartikmakhloga20@gmail.com>
2023-02-19 20:29:15 +0530

    addede bmi calculator

--- 3_calculator/calculator.js
+++ 3_calculator/calculator.js
@@ -2,18 +2,29 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 let app = express();
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/",function(req,res){
-    res.sendFile(__dirname + "/index.html");
-})
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
 
-app.post("/",function(req,res){
-   let num1 = Number(req.body.num1);
-   let num2 = Number(req.body.num2);
-   let result = num1 + num2;
-    res.send("The Result is " + result);
-})
-app.listen(3001,function(){
-    console.log("listening at 3001");
-})
+app.post("/", function (req, res) {
+  let num1 = Number(req.body.num1);
+  let num2 = Number(req.body.num2);
+  let result = num1 + num2;
+  res.send("The Result is " + result);
+});
+
+app.get("/bmicalculator", function (req, res) {
+  res.sendFile(__dirname + "/bmicalculator.html");
+});
+
+app.post("/bmicalculator", function (req, res) {
+  let height = Number(req.body.height);
+  let weight = Number(req.body.weight);
+  let bmiresult = weight / (height * height);
+  res.send("Your BMI is " + bmiresult);
+});
+app.listen(3001, function () {
+  console.log("listening at 3001");
+});
