e616afb5faf74238294468f91efddb1ece4aaa45
kartik <kartikmakhloga20@gmail.com>
2023-02-20 21:11:45 +0530

    added weather api dynamic

--- 4_WeatherProject/app.js
+++ 4_WeatherProject/app.js
@@ -1,11 +1,28 @@
 const express = require("express");
 const https = require("https");
+const bodyParser = require("body-parser");
 
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", function (req, res) {
+  
+
+  const query = req.body.cityName;;
+  const apiKey = "524901&appid=d8c54bf4c262d7afb068f93cd26a8c40";
+  const unit = "metric";
   let url =
-    "https://api.openweathermap.org/data/2.5/forecast?q=Paris&id=524901&appid=d8c54bf4c262d7afb068f93cd26a8c40&units=metric";
+    "https://api.openweathermap.org/data/2.5/forecast?q=" +
+    query +
+    "&id=" +
+    apiKey +
+    "&units=" +
+    unit;
   https.get(url, function (response) {
     response.on("data", function (data) {
       const weatherData = JSON.parse(data);
@@ -14,10 +31,10 @@ app.get("/", function (req, res) {
       const icon = weatherData.list[0].weather[0].icon;
       const imageURL = "https://openweathermap.org/img/wn/" + icon + "@2x.png";
       res.write(
-        "<h1>The temperature in Paris is " + temp + " degrees celcius.</h1>"
+        "<h1>The temperature in " + query + " is " + temp + " degrees celcius.</h1>"
       );
-      res.write("<p>The weather is currently" + weatherDesc + "</p>");
       res.write("<img src=" + imageURL + ">");
+      res.write("<p>The weather is currently " + weatherDesc + "</p>");
       res.send();
     });
   });
